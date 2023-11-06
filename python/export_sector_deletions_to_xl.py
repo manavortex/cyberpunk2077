650f1e5da0ea7244164df3aeb52d57f7880f7504
manavortex <manavortex@gmail.com>
2023-11-06 21:43:06 +0100

    Update export_sector_deletions_to_xl.py

--- python/export_sector_deletions_to_xl.py
+++ python/export_sector_deletions_to_xl.py
@@ -1,8 +1,23 @@
 import bpy
 
-# Specify the filename where you want to save the output.
-# Make sure to use an existing folder!
-output_filename = "C:\\Games\\Cyberpunk 2077\\archive\\pc\\mod\\tutorial_archivexl_sector_deletions.archive.xl"
+wolvenkit_project = "apartment_dogtown_cleaned_up"
+mod_directory = "C:\\Games\\Cyberpunk 2077\\archive\\pc\\mod\\"
+project_directory = "F:\\CyberpunkFiles\\world_editing\\"
+
+export_to_mod_dir = True
+is_DLC = False
+consider_partial_deletions = False
+
+delete_partials = [
+    # [ "soda_can", "trash" ],
+    # [ "squat_clothes" ],
+]
+
+# Specify the filename where you want to save the output. Make sure to use an existing folder!
+output_filename = f"{project_directory}\\{wolvenkit_project}\\source\\resources\\{wolvenkit_project}.archive.xl"
+
+if export_to_mod_dir:
+    output_filename = f"{mod_directory}\\{wolvenkit_project}.archive.xl"
 
 # For indenting your .xl file
 indent="  "
@@ -21,19 +36,33 @@ def countChildNodes(collection):
 # Function to find collections without children (these contain deletions)
 def find_empty_collections(collection):
     empty_collections = []
-    if len(collection.children) == 0 and len(collection.objects) == 0 and 'nodeIndex' in collection and 'nodeType' in collection:
-        empty_collections.append(collection)
+    is_deletion_candidate = 'nodeIndex' in collection and 'nodeType' in collection
+    if len(collection.children) == 0 and is_deletion_candidate:
+        if len(collection.objects) == 0:
+            empty_collections.append(collection)
+        if consider_partial_deletions and len(collection.children) > 0 and not collection.children[0]["Name"].startswith("submesh_00"):
+                empty_collections.append(collection)
+    elif is_deletion_candidate:
+        for deletion_check in delete_partials:
+            if all(partial in collection.name for partial in deletion_check):
+                empty_collections.append(collection)
+        
     for child_collection in collection.children:
         empty_collections.extend(find_empty_collections(child_collection))
+        
+
     return empty_collections
 
+sectorPath = "base\\worlds\\03_night_city\\_compiled\\default"
+if is_DLC:
+    sectorPath = f"{sectorPath}\\ep1"
 # Function to write to a text file
 def to_archive_xl(filename):
     with open(filename, "w") as file:
         file.write("streaming:\n")
         file.write(f"{indent}sectors:\n")
         for sectorName in deletions:
-            file.write(f"{indent}{indent}- path: base\\worlds\\03_night_city\\_compiled\\default\\{sectorName}\n")
+            file.write(f"{indent}{indent}- path: {sectorPath}\\{sectorName}\n".replace("ep1\\ep1", "ep1"))
             file.write(f"{indent}{indent}{indent}expectedNodes: {expectedNodes[sectorName]}\n")
             file.write(f"{indent}{indent}{indent}nodeDeletions:\n")
             sectorData = deletions[sectorName]
