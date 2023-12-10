3f34accfad49a7fad3c5522a98fd77f184064e93
manavortex <manavortex@gmail.com>
2023-12-10 18:04:00 +0100

    Update convert_sector_edits_to_AXL_1.9.py

--- python/world_sector_stuff/convert_sector_edits_to_AXL_1.9.py
+++ python/world_sector_stuff/convert_sector_edits_to_AXL_1.9.py
@@ -1,34 +1,12 @@
-import os
-import yaml
-import json
-
-# Converts your node removal .xl files for ArchiveXL 1.9
-# Script frankensteined by me from Simarillius's code in the Blender plugin, then edited by me, then edited by Sim, then edited by me.
-# Wouldn't exist without him, so a million thanks <3
-
-
-# the wolvenkit project with your sector json files
-wolvenkit_project="F:\\CyberpunkFiles\\world_editing\\Apartment_Loft_cleaned_up"
-
-# the file that you want to convert
-original_file="F:\\CyberpunkFiles\\world_editing\\apartment_loft_cleaned_up\\source\\resources\\apartment_loft_cleaned_up.archive.xl"
-
-# the file that you want to write to
-output_file="F:\\CyberpunkFiles\\world_editing\\apartment_loft_cleaned_up\\source\\resources\\apartment_loft_cleaned_up.converted.archive.xl"
-
-
-# For indenting your .xl file
-indent="  "
-
 import bpy
 import re
 import os
 
-wolvenkit_project = "apartment_loft_cleaned_up"
+wolvenkit_project = "_world_editing_debug_project"
 mod_directory = "C:\\Games\\Cyberpunk 2077\\archive\\pc\\mod\\"
 project_directory = "F:\\CyberpunkFiles\\world_editing\\"
 
-export_to_mod_dir = True
+export_to_mod_dir = False
 consider_partial_deletions = True
 
 # Specify the filename where you want to save the output. Make sure to use an existing folder!
@@ -46,7 +24,7 @@ delete_partials = [
 ]
 
 # if an item matches all strings in one of the sub-arrays, keep it. Supports regular expression.
-keep_partials = [ 
+keep_partials = [
     [ "^q\d\d" ],
     # ["entropy_lamp.*"]
 ]
@@ -102,30 +80,28 @@ def to_archive_xl(filename):
         file.write("streaming:\n")
         file.write(f"{indent}sectors:\n")
         for sectorPath in deletions:
+            sectorData = deletions[sectorPath]
+            
             # skip empty sectors/collections
-            if deletions[sectorPath] is None or len(deletions[sectorPath]) == 0:
+            if sectorData is None or len(sectorData) == 0:
                 continue
-                        
-            sectorName =  os.path.splitext(os.path.basename(sectorPath))[0]
+
+            sectorName = os.path.splitext(os.path.basename(sectorPath))[0]
+
             file.write(f"{indent}{indent}- path: base\\worlds\\03_night_city\\_compiled\\default\\{sectorName}\n")
             file.write(f"{indent}{indent}{indent}expectedNodes: {expectedNodes[sectorPath]}\n")
             file.write(f"{indent}{indent}{indent}nodeDeletions:\n")
-            sectorData = deletions[sectorPath]
-            
-            if "interior_-24_-16_1_1" in sectorName:
-                print(sectorData)
-            
+
+
             currentNodeIndex = sectorData[0]["nodeDataIndex"]
             currentNodeComment = ''
             currentNodeType = sectorData[0]["nodeType"]
             
-            # keep a counter so that we know when to always print node data - see quest_ca115e9713d725d7.streamingsector
-            numNodes = 1
-            
-            for empty_collection in sectorData:                
-                if empty_collection["nodeDataIndex"] > currentNodeIndex or numNodes == len(sectorData):
+            # keep track of how many nodes we have removed so that we don't swallow the last node
+            counter = 1
+            for empty_collection in sectorData:
+                if empty_collection["nodeDataIndex"] > currentNodeIndex or counter == len(sectorData):
                     # new node! write the old one
-                    file.write(f"{indent}{indent}{indent}{indent}# {currentNodeComment}\n")
                     file.write(f"{indent}{indent}{indent}{indent}- index: {currentNodeIndex}\n")
                     file.write(f"{indent}{indent}{indent}{indent}{indent}type: {currentNodeType}\n")
                     file.write(f"{indent}{indent}{indent}{indent}{indent}debugName: {currentNodeComment}\n")
@@ -137,12 +113,12 @@ def to_archive_xl(filename):
                 elif empty_collection["nodeDataIndex"] == currentNodeIndex:
                     prefix = ", " if currentNodeComment != "" else ""
                     currentNodeComment =  f"{currentNodeComment}{prefix}{empty_collection.name}"
-                # increment counter
-                numNodes += 1
+                counter += 1
+
 
 # Iterate over matching collections and find empty ones
 for sectorCollection in [c for c in bpy.data.collections if c.name.endswith("streamingsector")]:
-    file_path = sectorCollection["filepath"]    
+    file_path = sectorCollection["filepath"]
     expectedNodes[file_path] = countChildNodes(sectorCollection)
     collections = find_empty_collections(sectorCollection)
     if len(collections) > 0:
