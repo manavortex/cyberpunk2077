96e0a87c5885707a3d1746a5bd7dda2201f8e1ce
manavortex <manavortex@gmail.com>
2024-02-11 13:00:18 +0100

    Update cyberpunk2077_disable_all_mods_NEW_WIP.bat

--- batch/cyberpunk2077_disable_all_mods_NEW_WIP.bat
+++ batch/cyberpunk2077_disable_all_mods_NEW_WIP.bat
@@ -3,10 +3,10 @@ setlocal enabledelayedexpansion
 
 REM ===================================================================================================================
 REM helper script for troubleshooting: https://wiki.redmodding.org/cyberpunk-2077-modding/help/users-troubleshooting
-REM Up-to-date with 2.1.1
+REM Up-to-date with 2.1.1, but this won't go bad
 REM -------------------------------------------------------------------------------------------------------------------
 REM Debug mode?
-set DEBUG_MODE=0
+set DEBUG_MODE=1
 REM ===================================================================================================================
 
 REM for indenting user output
@@ -29,10 +29,8 @@ REM Make sure that we're in the cyberpunk directory
 REM yell at user if we aren't
 REM ==================================================================
 
-pushd "!CYBERPUNKDIR!"
-
 REM make sure that we're inside the cyberpunk dir
-if not exist ".\bin\x64\Cyberpunk2077.exe" (
+if not exist "!CYBERPUNKDIR!\bin\x64\Cyberpunk2077.exe" (
   echo.
   echo !separator!
   echo !tab!File not found !CYBERPUNKDIR!\bin\x64\Cyberpunk2077.exe
@@ -42,20 +40,20 @@ if not exist ".\bin\x64\Cyberpunk2077.exe" (
   echo !tab!Aborting...
   echo !separator!
   pause
-  goto :end
+  goto :eof
 )
 
 
 REM Create directory for backups
-if not exist ".\_MOD_REMOVER_BACKUPS" (
-  mkdir ".\_MOD_REMOVER_BACKUPS"
+if not exist "!CYBERPUNKDIR!\_MOD_REMOVER_BACKUPS" (
+  mkdir "!CYBERPUNKDIR!\_MOD_REMOVER_BACKUPS"
 )
 
 REM Initialize counter variable
 set /a count=0
 
 REM Count the number of subdirectories
-for /f %%i in ('dir /b /ad ".\_MOD_REMOVER_BACKUPS" 2^>nul ^| find /c /v ""') do set count=%%i
+for /f %%i in ('dir /b /ad "!CYBERPUNKDIR!\_MOD_REMOVER_BACKUPS" 2^>nul ^| find /c /v ""') do set count=%%i
 
 REM Check if the count is greater than 5
 if %count% gtr 5 (
@@ -63,11 +61,10 @@ if %count% gtr 5 (
 ) 
 
 set TIMESTAMP=
-for /f "tokens=1-5 delims=/: " %%d in ("%date% %time%") do (
-    set TIMESTAMP=%%d_%%e_%%f_%%g_%%h
-)
+REM Get timestamped string using PowerShell and store it in a variable
+for /f %%i in ('powershell Get-Date -Format "yyyy-MM-dd_HH-mm-ss"') do set "TIMESTAMP=%%i"
 
-set "BACKUP_DIRECTORY=.\_MOD_REMOVER_BACKUPS\!TIMESTAMP!"
+set "BACKUP_DIRECTORY=!CYBERPUNKDIR!\_MOD_REMOVER_BACKUPS\!TIMESTAMP!"
 if exist "!BACKUP_DIRECTORY!" (
   move "!BACKUP_DIRECTORY!" "!BACKUP_DIRECTORY!_old"
 )
@@ -103,37 +100,8 @@ REM pop them in an array for proper iterating
 set numRenamedFiles=0
 set "renamed_paths_list="
 
-for %%P in (%rename_paths%) do (
-	set "absolute_source=.\%%~P"
-	set "absolute_dest=.\!BACKUP_DIRECTORY!\%%~P"
-    
-    if exist "!absolute_source!" (
-        REM Check if the source is a directory
-        if exist "!absolute_source!\." (
-            REM Move directory and create the destination directory structure if needed
-			if "%DEBUG_MODE%"=="1" (
-				robocopy "!absolute_source!" "!absolute_dest!" /E /CREATE /njh /njs /ndl /nc /ns > nul
-			) else (
-				robocopy "!absolute_source!" "!absolute_dest!" /E /MOVE /CREATE /njh /njs /ndl /nc /ns > nul
-			)
-			if not exist "!absolute_source!" (
-				mkdir "!absolute_source!"
-			)
-        ) else (
-            REM Move file and create the destination directory if needed
-            mkdir "!BACKUP_DIRECTORY!\%%~P\.."
-			
-            REM Move directory and create the destination directory structure if needed
-			if "%DEBUG_MODE%"=="1" (
-				copy "!absolute_source!" "!absolute_dest!"
-			) else (
-				move "!absolute_source!" "!absolute_dest!"		
-			)
-        )
-    )
-	REM append it to array so we can print it later
-	set /A numRenamedFiles+=1
-	set renamed_paths_list[!numRenamedFiles!]=%%~P
+for %%P in (%rename_paths%) do (    
+  call :backup_file_or_folder %%~P
 )
 
 REM pop them in an array for proper iterating
@@ -141,20 +109,24 @@ set numDeletedFiles=0
 set "deleted_paths_list="
 
 for %%P in (!delete_paths!) do (  
-  set "absolute_path=.\%%~P"
+  set "absolute_path=!CYBERPUNKDIR!\%%~P"
   if exist "!absolute_path!" (
 	set /A numDeletedFiles+=1
 	set deleted_paths_list[!numDeletedFiles!]=%%~P
-	call :delete_file_or_folder_without_prompt "!absolute_path!"
+	if exist !absolute_path!\* (
+		rd /s /q !absolute_path!
+	  ) else (      
+	   del /f /q !absolute_path!
+	  )
   )
 )
 
 REM now that we're done deleting, make sure that r6\cache\modded exist to prevent file access errors
-if not exist ".\r6\cache" (
-  mkdir ".\r6\cache"
+if not exist "!CYBERPUNKDIR!\r6\cache" (
+  mkdir "!CYBERPUNKDIR!\r6\cache"
 )
-if not exist ".\r6\cache\modded" (
-  mkdir ".\r6\cache\modded"
+if not exist "!CYBERPUNKDIR!\r6\cache\modded" (
+  mkdir "!CYBERPUNKDIR!\r6\cache\modded"
 )
 
 REM =================================================================
@@ -178,15 +150,15 @@ if (!numDeletedFiles!) gtr (0) (
   echo The following outdated files were deleted: >> "!LOGFILE!"
   echo !separator! >> "!LOGFILE!"
   for /L %%i in (1,1,%numDeletedFiles%) do echo !tab!!deleted_paths_list[%%i]! >> "!LOGFILE!"
-  echo. >> "!LOGFILE!"
+  echo. >> "!LOGFILE!"
 )
 
-echo !separator! >> "!LOGFILE
+echo !separator! >> "!LOGFILE!
 echo. >> "!LOGFILE!"
 echo .
 
 
-echo You can find this output in !LOGFILE!
+echo You can find the full list in !LOGFILE!
 echo For further help, check https://wiki.redmodding.org/cyberpunk-2077-modding/for-mod-users/user-guide-troubleshooting#a-fresh-install-starting-from-scratch
 echo .
 echo " !separator! "
@@ -203,41 +175,64 @@ pause
 
 explorer !BACKUP_DIRECTORY!
 
-goto :end
+goto :eof
 
-:end
-	popd
-	goto :eof
 
-  
-:delete_file_or_folder_without_prompt
-  set "file=%*"
-  if not exist !file! goto :eof
-  
-  if exist !file!\* (
-    rd /s /q !file!
-  ) else (      
-   del /f /q !file!
-  )
-  goto :eof  
+:backup_file_or_folder
+	set "relative_path=%*"
+
+	set "source=!CYBERPUNKDIR!\!relative_path!"
+	set "dest=!BACKUP_DIRECTORY!\!relative_path!"
+	
+	if not exist "!source!" goto :eof
+			
+	REM Check if the source is a directory
+	if exist "!source!\*" (
+		REM Use powershell to recursively copy directory because it doesn't suck
+		powershell.exe -Command "Copy-Item -Path '!source!' -Destination '!dest!' -Recurse -Force"
+		
+		if "!DEBUG_MODE!"=="1" goto :log_file_operation	
+		
+		REM delete directory and create an empty folder
+		rd /s /q "!source!"
+		mkdir "!source!"
+	) else (	
+		REM Create the destination directory if needed
+		if not exist "!dest!\.." (
+			mkdir "!dest!"
+		)
+		powershell.exe -Command "Copy-Item -Path '!source!' -Destination '!dest!' -Force"		
+		if "!DEBUG_MODE!"=="1" goto :log_file_operation	
+		
+		del /f /q "!source!"
+	)
+
+	REM no goto eof here, we want to run through this even without goto 
+	
+	:log_file_operation
+		REM append it to array so we can print it later
+		set /A numRenamedFiles+=1
+		set renamed_paths_list[!numRenamedFiles!]=!relative_path!
+		goto :eof
+
 
 :delete_old_backups
 
 	echo You have more than 5 old mod remover backups under !CYBERPUNKDIR!\_MOD_REMOVER_BACKUPS.	
-		echo You really should delete them before your disk runs full.
-		set /p user_input="Delete files now? [Y] "
-		
-		if /i not "%user_input%"=="Y" goto :eof 
-		
-		echo.
-		echo !separator!
-		echo Script will delete your old backups now. Last chance to back them up!
-		echo.
-		pause
-		for /d %%i in ("!CYBERPUNKDIR!\_MOD_REMOVER_BACKUPS\*") do (
-			rd /s /q "%%i"
-		)
-		goto :eof
+	echo You really should delete them before your disk runs full.
+	set /p user_input="Delete files now? [Y] "
+	
+	if /i not "%user_input%"=="Y" goto :eof 
+	
+	echo.
+	echo !separator!
+	echo Script will delete your old backups now. Last chance to back them up!
+	echo.
+	pause
+	for /d %%i in ("!CYBERPUNKDIR!\_MOD_REMOVER_BACKUPS\*") do (
+		rd /s /q "%%i"
+	)
+	goto :eof
 		
 
 endlocal
