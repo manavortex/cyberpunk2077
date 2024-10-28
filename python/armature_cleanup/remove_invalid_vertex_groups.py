4e3041b83e56523109fb803c68284434f3e0b0a8
manavortex <manavortex@gmail.com>
2024-10-28 16:20:09 +0100

    Update remove_invalid_vertex_groups.py

--- python/armature_cleanup/remove_invalid_vertex_groups.py
+++ python/armature_cleanup/remove_invalid_vertex_groups.py
@@ -1,16 +1,16 @@
 import bpy
 
 # credit goes to lucky: https://blenderartists.org/t/how-to-delete-useless-vertex-groups-from-deleted-bones/1185075/8
-def delete_nodeform_vgroups():
+def delete_nodeform_vgroups(mesh):
     arm = None
-    arm = bpy.context.active_object.find_armature()
+    arm = mesh.find_armature()
     if arm == None:
         print("No armature modifier found, add one")
         return
     
     bones_list = [b.name for b in arm.data.bones if b.use_deform]
     
-    obj = bpy.context.active_object
+    obj = mesh
     
     if len(obj.vertex_groups) == 0:
         print("No vertex groups found")
@@ -22,4 +22,8 @@ def delete_nodeform_vgroups():
                 print("Non deformable vertex group found:", vgroup.name)
                 obj.vertex_groups.remove(vgroup)
 
-delete_nodeform_vgroups()
+
+# Iterate through all selected mesh objects
+for obj in bpy.context.selected_objects:
+    if obj.type == 'MESH':
+        delete_nodeform_vgroups(obj)
